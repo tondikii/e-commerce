b879e87d3aba41208793feaa1df81edd6ed0b9dc
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2025-09-01 00:45:05 +0700

    add config experimental missingSuspenseWithCSRBailout

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,9 @@ const nextConfig = {
   },
   reactStrictMode: false,
   crossOrigin: "anonymous",
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
