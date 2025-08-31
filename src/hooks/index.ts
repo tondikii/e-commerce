e9d002e3d90a0b123c79e22a168064f6474ac6d6
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2025-09-01 00:36:52 +0700

    remove hooks useSearchParams

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export {default as useRedirectIfAuthenticated} from "./useRedirectIfAuthenticated";
 export {default as useFetch} from "./useFetch";
-export {default as useSearchParams} from "./useSearchParams";
 export {default as useProduct} from "./useProduct";
 export {default as useCart} from "./useCart";
